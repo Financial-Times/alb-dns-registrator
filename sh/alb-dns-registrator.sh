31fa5251b54d841c40836c1464fffad1ee8af906
Jussi.Heinonen <jussi.heinonen@ft.com>
2017-02-03 10:11:04 +0000

    Add logic to create/delete DNS record

--- sh/alb-dns-registrator.sh
+++ sh/alb-dns-registrator.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
-
+#
 # Query EC2 instance tag aws:cloudformation:stack-name value based on instance ID
 # With aws:cloudformation:stack-name query ALB DNSName and update DynDNS record for
 # ${aws:cloudformation:stack-name}-read-alb-up.ft.com and
 # ${aws:cloudformation:stack-name}-write-alb-up.ft.com
-# Author: jussi.heinonen@ft.com - 27.1.2017
+# © jussi.heinonen@ft.com - 2.2.2017
 #
 source $(dirname $0)/functions.sh || echo "$(date '+%x %X') $0: Failed to source functions.sh"
+
+DOMAIN="ft.com"
+TTL="60"
+
 
 usage() {
   echo "USAGE: $0  [--debug=true] [--instance-id=i-0123abc] [--region=aws-region] --dynkey=sercret"
@@ -22,5 +26,27 @@ STACKNAME=$(getStackName)
 if [[ ${STACKNAME} == '' ]]; then
   errorAndExit "Failed to get stack-name. Exit 1." 1
 else
-  [[ "${ARGS[--debug]}" ]] &&  info "Stack name is ${STACKNAME}"
+  READALBCNAME="${STACKNAME}-read-alb-up"
+  WRITEALBCNAME="${STACKNAME}-write-alb-up"
+  if [[ "${ARGS[--debug]}" ]]; then
+    info "Stack name: ${STACKNAME}"
+    info "Read ALB CNAME: ${READALBCNAME}.${DOMAIN}"
+    info "Write ALB CNAME: ${WRITEALBCNAME}.${DOMAIN}"
+  fi
+fi
+
+READALB=$(getAlbDnsName ${STACKNAME}-read-alb)
+if [[ ${READALB} == '' ]]; then
+  errorAndExit "Failed to get DNS name for ${READALBCNAME}. Exit 1." 1
+else
+  [[ "${ARGS[--debug]}" ]] &&  info "DNS name for ${READALBCNAME}: ${READALB}"
+  cnameCreateOrUpdate ${READALBCNAME} ${DOMAIN} ${READALB} ${TTL}
+fi
+
+WRITEALB=$(getAlbDnsName ${STACKNAME}-write-alb)
+if [[ ${WRITEALB} == '' ]]; then
+  errorAndExit "Failed to get DNS name for ${WRITEALBCNAME}. Exit 1." 1
+else
+  [[ "${ARGS[--debug]}" ]] &&  info "DNS name for ${WRITEALBCNAME}: ${WRITEALB}"
+  cnameCreateOrUpdate ${WRITEALBCNAME} ${DOMAIN} ${WRITEALB} ${TTL}
 fi
--- sh/functions.sh
+++ sh/functions.sh
@@ -1,6 +1,45 @@
 declare -A ARGS
 declare -r CURL_CMD="curl -s --connect-timeout 3"
 
+cnameCreateOrUpdate() {
+  RECORD="${1}"
+  ZONE="${2}"
+  FQDN="${1}.${2}"
+  ALBDNS="${3}"
+  TTL="${4}"
+  if [[ "$(cnameExist ${FQDN})" ]]; then
+    if [[ "$(cnameUpToDate ${FQDN} ${ALBDNS})" ]]; then
+      info "Cname ${FQDN} is up to date"
+    else
+      info "Cname ${FQDN} requires update"
+      deleteCname ${ZONE} ${RECORD}
+      createCname ${ZONE} ${RECORD} ${ALBDNS} ${TTL}
+    fi
+  else
+    warn "CNAME ${FQDN} does not exist. Creating CNAME."
+    createCname ${ZONE} ${RECORD} ${ALBDNS} ${TTL}
+  fi
+}
+
+cnameExist() {
+  #ARG1: cname to lookup
+  #returns: true if cname exists
+  host -t cname $1 &>/dev/null && echo "true"
+}
+
+cnameUpToDate() {
+  host -t cname $1 | grep -o $2 && echo "true"
+}
+
+createCname() {
+  info "Creating record zone: $1, name: $2, record: $3, ttl: $4"
+  python $(dirname $0)/../python/create-dns -z $1 -n $2 -r $3 -t $4 -k ${ARGS[--dynkey]} || errorAndExit "Failed to create CNAME for $2.$1 Exit 1." 1
+}
+
+deleteCname() {
+  python $(dirname $0)/../python/delete-dns -z $1 -n $2 -k ${ARGS[--dynkey]} || errorAndExit "Failed to delete CNAME for $2.$1 Exit 1." 1
+}
+
 errorAndExit() {
   echo -e "\e[31m$(date '+%x %X') ERROR: $1\e[0m"
   exit $2
@@ -8,6 +47,16 @@ errorAndExit() {
 
 info() {
   echo -e "\e[34m$(date '+%x %X') INFO: ${1}\e[0m"
+}
+
+getAlbDnsName() {
+  #ARG1: name of the ALB to LOOKUP
+  #returns: ALB DNS name
+  if [[ -z $1 ]]; then
+    errorAndExit "${FUNCNAME}: ALB name must be provided. Exit 1." 1
+  else
+    aws elbv2 describe-load-balancers --region ${ARGS[--region]} --name $1 --query="LoadBalancers[].DNSName" --output text
+  fi
 }
 
 getInstanceId() {
